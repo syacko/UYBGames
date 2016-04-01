303016c03e2a249fd69806255e89401c94535237
Scott Yacko <scott@yackofamily.com>
2016-04-01 09:50:44 -0700

    Corrected typo in parameters.yml to css_library from csss_library Add getParameter calls to AppBundle:DefaultController for js and css libraries.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,6 +16,8 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            'jsLibrary' => $this->getParameter('js_library'),
+            'cssLibrary' => $this->getParameter('css_library'),
         ]);
     }
 }
