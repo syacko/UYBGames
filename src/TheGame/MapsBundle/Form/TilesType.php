5d04a12bdaae2c3f0d3e7ce523affc995ab63adf
Scott Yacko <scott@yackofamily.com>
2016-04-03 21:43:47 -0700

    Fixed show and edit.html.twig formatting issues Added formatting to TilesType.php Updated <th> css to have nowrap

--- src/TheGame/MapsBundle/Form/TilesType.php
+++ src/TheGame/MapsBundle/Form/TilesType.php
@@ -15,12 +15,22 @@ class TilesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('mapId')
-            ->add('tileColRow')
-            ->add('tileSectorName')
-            ->add('tilePlayable')
-            ->add('tileData')
-        ;
+            ->add('mapId', null, array('label' => 'Map Id',
+                'label_attr' => array('class' => 'UYBGFont_1em UYBGLJustify UYBGFont_Bold', 'style' => 'margin-right: 95px;'),
+                'attr' => array('class' => 'UYBGListBorder UYBGFont_075em', 'style' => 'width: 30%;')))
+            ->add('tileColRow', null, array('label' => 'Column and Row',
+                'label_attr' => array('class' => 'UYBGFont_1em UYBGLJustify UYBGFont_Bold', 'style' => 'margin-right: 2px;'),
+                'attr' => array('class' => 'UYBGListBorder UYBGFont_075em')))
+            ->add('tileSectorName', null, array('label' => 'Sector Name',
+                'label_attr' => array('class' => 'UYBGFont_1em UYBGLJustify UYBGFont_Bold', 'style' => 'margin-right: 35px;'),
+                'attr' => array('class' => 'UYBGListBorder UYBGFont_075em')))
+            ->add('tilePlayable', null, array('label' => 'Playable',
+                'label_attr' => array('class' => 'UYBGFont_1em UYBGLJustify UYBGFont_Bold', 'style' => 'margin-right: 73px;'),
+                'attr' => array('class' => 'UYBGListBorder UYBGFont_075em')))
+            ->add('tileData', null, array('label' => 'Tile JSON',
+                'label_attr' => array('class' => 'UYBGFont_1em UYBGLJustify UYBGFont_Bold',
+                    'style' => 'margin-right: 63px;'),
+                'attr' => array('class' => 'UYBGListBorder UYBGFont_075em', 'style' => 'width: 30%; height: 75px; margin-top: 2px;')));
     }
     
     /**
