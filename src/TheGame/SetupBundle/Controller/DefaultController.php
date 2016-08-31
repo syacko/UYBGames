216b0e66b0277598e2f1446a4cf62d6aa055704c
syacko <scott@yackofamily.com>
2016-08-31 09:20:20 -0700

    Changed page layout and created thegame_setup.css file

--- src/TheGame/SetupBundle/Controller/DefaultController.php
+++ src/TheGame/SetupBundle/Controller/DefaultController.php
@@ -14,7 +14,7 @@ class DefaultController extends Controller
     {
 //        return $this->render('GameSetupBundle:Default:index.html.twig'); DO NOT USE
 
-        return $this->render('TheGameSetupBundle:Default:setup.html.twig',
+        return $this->render('setup.html.twig.test',
             array('jsLibrary' => $this->getParameter('js_library'),
                 'cssLibrary' => $this->getParameter('css_library'),
                 'imgLibrary' => $this->getParameter('img_library'),
